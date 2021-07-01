fd1d3c58f1a8bd139e0bb840e48dcba19f1730e2
javierpisochin <49105053+javierpisochin@users.noreply.github.com>
2021-07-01 15:47:16 -0300

    Actividad práctica obligatoria

---
+++ actividad.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<h1>Javier</h1>";
+echo "<h1>Pisochin</h1>";
+echo "<h2>44 años</h2>";
+
+echo "<br>";
+echo "estudiar cosas que me interesan, edicion de video";
+echo "<br>";
+echo "visual studio";
+echo "<br>";
+echo "windows 10";
+
+?>
